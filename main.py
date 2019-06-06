b17b78226ab5503f1a2ce8b6e9804e78a111289d
John Franklin <franklify@gmail.com>
2019-06-05 17:05:35 -0700

    Adds logic to find plant color

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ the_jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+color_relationships = {
+    'Black': ['Grey', 'White'],
+    'Green': ['Red', 'Purple'], 
+    'Grey': ['Black', 'White'],
+    'Purple': ['Yellow'],
+    'Red': ['Green'],
+    'Yellow': ['Purple'],
+    'White': ['Black', 'Grey']
+}
+
 # the handler section
 class MainPage(webapp2.RequestHandler):
     def get(self): #for a get request
@@ -23,9 +33,17 @@ class ResultPage(webapp2.RequestHandler):
     #     self.response.write(breakfast_template.render(my_plant="Zanzibar Gem")) #the response
 
     def post(self):
-        breakfast_template = the_jinja_env.get_template('templates/result.html')
-        user_plant = self.request.get("userplant")
-        self.response.write(breakfast_template.render(my_plant=user_plant)) #the response
+        results_template = the_jinja_env.get_template('templates/result.html')
+        space_color = self.request.get("space_color")
+
+        plant_colors = color_relationships[space_color]
+        plant_colors_string = plant_colors[0]
+        for color in plant_colors[1:]:
+            plant_colors_string += " and " + color
+
+        plant_url = "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_snake-plant-slaurentii_terracotta_4_800x.jpg?v=1537308389"
+
+        self.response.write(results_template.render(my_plant=plant_colors_string, image_link=plant_url)) #the response
 
 # the app configuration section
 app = webapp2.WSGIApplication([
